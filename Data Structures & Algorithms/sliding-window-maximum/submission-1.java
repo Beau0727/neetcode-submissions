56b88421f4459e922dcfdad96fea79c7a3c9439d
Haobo Ma <2746042268@qq.com>
2026-07-08 16:53:23 +0800

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> q = new ArrayDeque<>();
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        int l = 0, r = 0;
+        while(r < n) {
+            while (!q.isEmpty() && nums[q.getLast()] < nums[r]) {
+                q.removeLast();
+            }
+            q.addLast(r);
+
+            if (l > q.getFirst()) {
+                q.removeFirst();
+            }
+            if (r + 1>= k) {
+                ans[l] = nums[q.getFirst()];
+                l++;
+            }
+            r++;
+        }
+        return ans;
+    }
+}
